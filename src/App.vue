000be18e83a59ea7d73b664bbed162c36f7c766c
muhammadjon2506 <vorz7337@gmail.com>
2025-04-30 16:57:05 +0500

    team-burger

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Home from "./components/Home.vue"
+import Category from "./components/Category.vue"
+import Cards from "./components/Cards.vue"
+import Footer from "./components/Footer.vue"
+
+export default {
+  components: {
+    Home,
+    Category,
+    Cards,
+    Footer
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Home />
+    <Category />
+    <Cards />
+    <Footer />
+  </div>
+</template>
+
+<style>
+/* Style yozishingiz mumkin */
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Cards</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
